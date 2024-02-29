6173b2b49af97d67e33684a72969c977561f3cfe
LenonFelipe <lenonfelipe98@gmail.com>
2024-02-29 00:53:56 -0300

    Mongo connection successfully

--- app.js
+++ app.js
@@ -15,9 +15,13 @@ app.get("/", (req,res) => {
 //ENV com dados do mongo
 require("dotenv").config();
 
+// Definindo como false para nao haver problemas  
+mongoose.set('strictQuery', false);
+
+
 //Conexão com MongoDB
 const Schema = mongoose.Schema;
-mongoose.connect(process.env.MONGO_DB,{
+mongoose.connect("mongodb+srv://lenonfelipe98:12348@cluster0.7c0ovrl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",{
     useNewUrlParser: true
 }).then(()=>{
     console.log("Mongo conectado");
